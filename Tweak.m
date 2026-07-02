05686bd2d3664268f8ca76873907e376342d399f
8vz4t865m8-sudo <8vz4t865m8@privaterelay.appleid.com>
2026-07-02 10:44:33 +0800

    Update Tweak.m

--- Tweak.m
+++ Tweak.m
@@ -3,26 +3,30 @@
 
 #define NEW_IP @"162.14.104.134"
 
-@interface MRCloudRelay : NSObject
-- (NSString *)publishWsUrl;
-@end
-
-@implementation MRCloudRelay (Hook)
-
-- (NSString *)new_publishWsUrl {
-    // 原来: ws://210.16.163.46:93/loon
-    // 改成: ws://162.14.104.134/ws?role=publisher&room=default
+// 新实现 - 直接返回新服务器地址
+static NSString *new_publishWsUrl(id self, SEL _cmd) {
     return @"ws://" NEW_IP "/ws?role=publisher&room=default";
 }
 
-@end
-
 __attribute__((constructor))
 static void init() {
+    // 运行时获取 MRCloudRelay 类
     Class relay = objc_getClass("MRCloudRelay");
-    if (relay) {
-        Method orig = class_getInstanceMethod(relay, @selector(publishWsUrl));
-        Method new = class_getInstanceMethod(relay, @selector(new_publishWsUrl));
-        method_exchangeImplementations(orig, new);
+    if (!relay) return;
+    
+    // 获取原方法
+    Method origMethod = class_getInstanceMethod(relay, @selector(publishWsUrl));
+    if (!origMethod) return;
+    
+    // 添加新方法
+    BOOL added = class_addMethod(relay, @selector(new_publishWsUrl), (IMP)new_publishWsUrl, method_getTypeEncoding(origMethod));
+    
+    if (added) {
+        // 交换实现
+        Method newMethod = class_getInstanceMethod(relay, @selector(new_publishWsUrl));
+        method_exchangeImplementations(origMethod, newMethod);
+    } else {
+        // 如果添加失败，直接替换实现
+        method_setImplementation(origMethod, (IMP)new_publishWsUrl);
     }
 }
