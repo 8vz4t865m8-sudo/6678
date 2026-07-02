01585093a1d279659851b6303dc7bee251836095
8vz4t865m8-sudo <8vz4t865m8@privaterelay.appleid.com>
2026-07-02 10:27:07 +0800

    Update Tweak.m

--- Tweak.m
+++ Tweak.m
@@ -1,34 +1,36 @@
+// Tweak.m - 不用 substrate
 #import <objc/runtime.h>
-#import <substrate.h>
+#import <Foundation/Foundation.h>
 
 #define NEW_IP @"162.14.104.134"
 
-static NSString *(*orig_directWatchUrl)(id, SEL);
+// 替换 directWatchUrl
 static NSString *hook_directWatchUrl(id self, SEL _cmd) {
     return [NSString stringWithFormat:@"http://%@/?game=dfm", NEW_IP];
 }
 
-static NSString *(*orig_publishWsUrl)(id, SEL);
+// 替换 publishWsUrl  
 static NSString *hook_publishWsUrl(id self, SEL _cmd) {
     return [NSString stringWithFormat:@"ws://%@/ws?role=publisher&room=default", NEW_IP];
 }
 
-static NSURL *(*orig_URLWithString)(id, SEL, NSString *);
+// 替换 URLWithString
 static NSURL *hook_URLWithString(id self, SEL _cmd, NSString *URLString) {
-    URLString = [URLString stringByReplacingOccurrencesOfString:@"210.16.163.46" withString:NEW_IP];
-    URLString = [URLString stringByReplacingOccurrencesOfString:@":93" withString:@""];
-    return orig_URLWithString(self, _cmd, URLString);
+    if ([URLString containsString:@"210.16.163.46"]) {
+        URLString = [URLString stringByReplacingOccurrencesOfString:@"210.16.163.46" withString:NEW_IP];
+        URLString = [URLString stringByReplacingOccurrencesOfString:@":93" withString:@""];
+    }
+    return ((NSURL *(*)(id, SEL, NSString *))class_getMethodImplementation([NSURL class], _cmd))(self, _cmd, URLString);
 }
 
 __attribute__((constructor))
 static void init() {
     Class relay = objc_getClass("MRCloudRelay");
     if (relay) {
-        MSHookMessageEx(relay, @selector(directWatchUrl), (IMP)hook_directWatchUrl, (IMP *)&orig_directWatchUrl);
-        MSHookMessageEx(relay, @selector(publishWsUrl), (IMP)hook_publishWsUrl, (IMP *)&orig_publishWsUrl);
+        method_setImplementation(class_getInstanceMethod(relay, @selector(directWatchUrl)), (IMP)hook_directWatchUrl);
+        method_setImplementation(class_getInstanceMethod(relay, @selector(publishWsUrl)), (IMP)hook_publishWsUrl);
     }
-    Class urlClass = objc_getClass("NSURL");
-    if (urlClass) {
-        MSHookMessageEx(urlClass, @selector(URLWithString:), (IMP)hook_URLWithString, (IMP *)&orig_URLWithString);
-    }
+    
+    Class urlClass = [NSURL class];
+    method_setImplementation(class_getClassMethod(urlClass, @selector(URLWithString:)), (IMP)hook_URLWithString);
 }
